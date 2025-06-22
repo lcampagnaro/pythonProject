a08ed0c766322bb5b0c1ed7e1a87350acec0af42
Leandro Campagnaro <57913956+lcampagnaro@users.noreply.github.com>
2025-06-22 19:47:12 -0300

    moeda e teste deve usar junto
    são 2 arquivos interligados para estudo de funções

---
+++ teste.py
@@ -0,0 +1,7 @@
+from Ex107 import moeda
+
+p = float(input('Digite o preço: R$'))
+taxaa = float(input('Digite a taxa de aumento necessário: '))
+taxar = float(input('Digite a taxa de desconto para cálcular: '))
+moeda.resumo(p, taxaa, taxar)
+
